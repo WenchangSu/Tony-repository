3bd59ba2d0d803441ea26b855fe628b8de36af76
Tony Chao <saasbook@saasbook.(none)>
2015-10-25 17:05:48 +0800

    #new is ok now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,15 @@
 class PostsController < ApplicationController
 
-
+	def index
+		@posts = Post.all
+	end
 
 	def show 
 		@post = Post.find(params[:id])
 	end
 
 	def new
-	end
-
-	def index
-		@posts = Post.all
+		@post = Post.new
 	end
 
 	def creat
